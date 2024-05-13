0f729c144f3ba4a3768af640cfaf365f705219f1
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-05-13 07:38:40 +0100

    refactor: Rename import statement for AProposDeMoiPage component in index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 //Pages
-import AProposDeMoiPage from "./propos";
+import AProposDeMoiPage from "./a-propos";
 import PortfolioPage from "../pages/portfolio";
 import ExperiencePage from "../pages/experience";
 import HomePage from "../pages/homepage";
 //Components
 import ServicesOffer from "@/components/ServicesOffer";
 import FooterPage from "@/components/FooterPage";
-import CarrouselTechsDivContainer from "@/components/CarrouselTechsDivContainer";
+
 
 export default function Index() {
 	return (
