a69d04d72f72de0165aa8fe79b89ebff6eb7abbb
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-05-13 07:32:18 +0100

    feat: Add CarrouselTechsDivContainer component for displaying technologies in a carousel

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 //Pages
-import AProposDeMoiPage from "../pages/a-propos";
+import AProposDeMoiPage from "./propos";
 import PortfolioPage from "../pages/portfolio";
 import ExperiencePage from "../pages/experience";
 import HomePage from "../pages/homepage";
 //Components
 import ServicesOffer from "@/components/ServicesOffer";
 import FooterPage from "@/components/FooterPage";
+import CarrouselTechsDivContainer from "@/components/CarrouselTechsDivContainer";
 
 export default function Index() {
 	return (
