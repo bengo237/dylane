9c46bb11a629e76807f57b2c8537586fe806314d
bengo237 <chaneldylanebengono@gmail.com>
2026-04-07 19:25:23 +0100

    fix: update Head component title and meta tags for improved SEO; add favicon.svg

--- components/Head.jsx
+++ components/Head.jsx
@@ -5,13 +5,14 @@ export default function HeadTag(props) {
 	const theme = useTheme();
 	return (
 		<Head>
-			<title>Bengo237| {props.title}</title>
+			<title>{props.title}</title>
 			<meta name="theme-color" content={theme.colors.branding} /> {/* <!-- Chrome, Firefox OS and Opera -->*/}
 			<meta name="msapplication-navbutton-color" content={theme.colors.branding} /> {/* <!-- Windows Phone -->*/}
 			<meta name="apple-mobile-web-app-status-bar-style" content={theme.colors.branding} /> {/*<!-- iOS Safari -->*/}
 			<meta name="description" content={props.metaDescription} />
 			<meta name="keywords" content={props.keywords} />
 			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 			<link rel="manifest" href="/site.webmanifest"></link>
